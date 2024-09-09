d673ed2e549d61596353fa1211756557b08103d3
Joseph Finney <josephfinney@live.com>
2024-09-08 22:05:48 -0500

    Add some navigation and theme upgrades to HTML view

--- miniLook/Views/HtmlViewWindow.xaml.cs
+++ miniLook/Views/HtmlViewWindow.xaml.cs
@@ -1,3 +1,7 @@
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
 using miniLook.Helpers;
 using Windows.UI.ViewManagement;
 
@@ -8,6 +12,7 @@ public sealed partial class HtmlViewWindow : WindowEx
     private Microsoft.UI.Dispatching.DispatcherQueue dispatcherQueue;
 
     private readonly UISettings settings;
+    private readonly bool IsDarkTheme;
 
     public HtmlViewWindow()
     {
@@ -36,5 +41,31 @@ public sealed partial class HtmlViewWindow : WindowEx
     {
         await WebView.EnsureCoreWebView2Async();
         WebView.NavigateToString(content);
+    }
+
+    private void WebView_CoreWebView2Initialized(WebView2 sender, CoreWebView2InitializedEventArgs args)
+    {
+        if (App.Current.RequestedTheme is ApplicationTheme.Dark)
+        {
+            WebView.DefaultBackgroundColor = Colors.DimGray;
+            WebView.CoreWebView2.Profile.PreferredColorScheme = CoreWebView2PreferredColorScheme.Dark;
+        }
+        else
+        {
+            WebView.DefaultBackgroundColor = Colors.LightGray;
+            WebView.CoreWebView2.Profile.PreferredColorScheme = CoreWebView2PreferredColorScheme.Light;
+        }
+
+        WebView.CoreWebView2.NewWindowRequested += CoreWebView2_NewWindowRequested;
+    }
+
+    private void CoreWebView2_NewWindowRequested(CoreWebView2 sender, CoreWebView2NewWindowRequestedEventArgs args)
+    {
+        args.Handled = true;
+        // No need to wait for the launcher to finish sending the URI to the browser
+        // before we allow the WebView2 in our app to continue.
+        _ = Windows.System.Launcher.LaunchUriAsync(new Uri(args.Uri));
+        // LaunchUriAsync is the WinRT API for launching a URI.
+        // Another option not involving WinRT might be System.Diagnostics.Process.Start(args.Uri);
     }
 }
